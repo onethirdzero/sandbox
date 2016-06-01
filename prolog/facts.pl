e3c1b0b3494f9f10d50d32b9d082d5038b3ac3fa
Jordan Siaw <jordansiaw.cy@gmail.com>
2016-05-31 22:33:40 -0700

    Add tutorial source URL.

--- prolog/facts.pl
+++ prolog/facts.pl
@@ -1,3 +1,6 @@
+% Tutorial found here:
+% https://bernardopires.com/2013/10/try-logic-programming-a-gentle-introduction-to-prolog/
+
 % relationship(arg1, arg2, ...).
 likes(alice, bob).
 likes(bob, carol).

24475b482fd999ed0bba3a8acdf9f0aad9010f77
Jordan Siaw <jordansiaw.cy@gmail.com>
2016-05-31 22:24:20 -0700

    Remove README.md.

--- prolog/facts.pl
+++ prolog/facts.pl
@@ -6,3 +6,22 @@ likes(mary, james).
 
 % conclusion(arg1, arg2, ...) :- relation1, relation2, ... , relationN.
 love_compatible(X, Y) :- likes(X, Y), likes(Y, X).
+
+mother(alice, lea).
+mother(john, julia).
+mother(lea, alberta).
+father(james, alfred).
+father(lea, john).
+
+% To Prolog, it doesn't matter if there are multiple definitions of a rule.
+% If the first clause fails, it just tries the second one, and so on.
+parent(X, Y) :- father(X, Y).
+parent(X, Y) :- mother(X, Y).
+
+% For Prolog, there is no difference between a rule and a fact.
+% A fact is a rule that is always true.
+
+% A rule can be defined in terms of another rule.
+
+% If X's parent is Z and Z's parent is Y, then X's grandparent is Y.
+grandparent(X, Y) :- parent(X, Z), parent(Z, Y).
